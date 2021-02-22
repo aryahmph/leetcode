967cf33fd8e339571d406f956f73fa84c87e6c46
Arya Yunanta <77351340+aryahmph@users.noreply.github.com>
2021-02-22 21:29:55 +0700

    Add files via upload

---
+++ TwoSumII_InputArrayIsSorted.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class TwoSumII_InputArrayIsSorted {
+
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers.length == 2)
+            return new int[] {0,0};
+        int left = 0, right = numbers.length-1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                return new int[] {left+1, right+1};
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
