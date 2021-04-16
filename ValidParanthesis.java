35a6e90d550ad82bb140d6e417c6738040f8320a
Arya Yunanta <77351340+aryahmph@users.noreply.github.com>
2021-04-16 10:34:57 +0700

    Add files via upload

---
+++ ValidParanthesis.java
@@ -0,0 +1,25 @@
+package com.leetcode;
+
+import java.util.Stack;
+
+public class ValidParanthesis {
+    public static boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+
+        return stack.isEmpty();
+    }
+}
